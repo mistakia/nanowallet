feb9504e73c5a1ba18241a41bb112f30c29058ab
mistakia <1823355+mistakia@users.noreply.github.com>
2021-03-16 13:43:29 -0400

    feat: transaction keyboard animations

--- src/views/pages/transaction/index.js
+++ src/views/pages/transaction/index.js
@@ -1,1 +1,12 @@
-export { default } from './transaction'
+import { connect } from 'react-redux'
+import { createSelector } from 'reselect'
+
+import { getSelectedAccount } from '@core/accounts'
+
+import TransactionPage from './transaction'
+
+const mapStateToProps = createSelector(getSelectedAccount, (account) => ({
+  account
+}))
+
+export default connect(mapStateToProps)(TransactionPage)
--- src/views/pages/transaction/transaction.native.js
+++ src/views/pages/transaction/transaction.native.js
@@ -1,88 +1,180 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
+import * as Animatable from 'react-native-animatable'
 import { View, Text, StyleSheet } from 'react-native'
 import { Button, TouchableRipple, IconButton } from 'react-native-paper'
 import Icon from 'react-native-vector-icons/MaterialIcons'
+import { BigNumber } from 'bignumber.js'
 
 import Logo from '@components/logo'
 import constants from '@core/constants'
 
-export default function () {
-  return (
-    <View style={styles.container}>
-      <View>
-        <IconButton
-          size={36}
-          color='white'
-          icon='crop-free'
-          onPress={() => console.log('Pressed')}
-        />
-      </View>
-      <View style={styles.amountContainer}>
-        <Logo color='white' size={80} />
-        <Text style={styles.amountText}>0</Text>
-      </View>
-      <View style={styles.keyboardContainer}>
-        <View style={styles.keyboardRow}>
-          <Button mode='text' onPress={() => console.log('Pressed')}>
-            <Text style={styles.keyboardButton}>1</Text>
-          </Button>
-          <Button mode='text' onPress={() => console.log('Pressed')}>
-            <Text style={styles.keyboardButton}>2</Text>
-          </Button>
-          <Button mode='text' onPress={() => console.log('Pressed')}>
-            <Text style={styles.keyboardButton}>3</Text>
-          </Button>
+export class KeyboardButton extends PureComponent {
+  ref = null
+
+  state = { fontSize: 24 }
+  handleRef = (ref) => (this.ref = ref)
+
+  handlePress = () => {
+    if (this.ref && this.props.onPress) {
+      this.setState({ fontSize: 40 })
+      this.ref.pulse(400)
+      this.props.onPress()
+    }
+  }
+
+  handleTransitionEnd = () => {
+    this.setState({ fontSize: 24 })
+  }
+
+  render() {
+    const { fontSize } = this.state
+    return (
+      <Animatable.View ref={this.handleRef} style={styles.keyboardButton}>
+        <Button
+          mode='text'
+          onPress={this.handlePress}
+          style={{ flex: 1 }}
+          contentStyle={{ height: '100%', width: '100%' }}>
+          {this.props.text ? (
+            <Animatable.Text
+              transition='fontSize'
+              duration={100}
+              style={{ ...styles.keyboardButtonText, fontSize }}
+              onTransitionEnd={this.handleTransitionEnd}>
+              {this.props.text}
+            </Animatable.Text>
+          ) : (
+            this.props.children
+          )}
+        </Button>
+      </Animatable.View>
+    )
+  }
+}
+
+export default class TransactionPage extends React.Component {
+  state = {
+    amount: [0]
+  }
+
+  handleViewRef = (ref) => (this.view = ref)
+
+  setBalance = (amount) => {
+    const { account } = this.props
+    const bal = amount.join('')
+    const bn = new BigNumber(bal)
+    const balanceRaw = bn.shiftedBy(30)
+    if (balanceRaw.isGreaterThan(account.balance)) {
+      return this.view.shake(200)
+    }
+
+    this.setState({ amount })
+  }
+
+  addAmount = (num) => {
+    if (this.state.amount.length === 1 && this.state.amount[0] === 0) {
+      const amount = [num]
+      return this.setBalance(amount)
+    }
+
+    const amount = [...this.state.amount, num]
+    this.setBalance(amount)
+  }
+
+  addDecimal = () => {
+    if (this.state.amount.includes('.')) {
+      return this.view.shake(200)
+    }
+
+    const amount = [...this.state.amount, '.']
+    this.setBalance(amount)
+  }
+
+  removeAmount = () => {
+    const arr = this.state.amount.slice(0, -1)
+
+    if (!arr.length) {
+      this.view.shake(200)
+      return this.setState({ amount: [0] })
+    }
+
+    this.setState({ amount: arr })
+  }
+
+  render() {
+    let fontSize = 60
+    const length = this.state.amount.length
+    if (length > 2) {
+      let t = length / 30
+      const ease = --t * t * t + 1
+      fontSize = 60 - ease * 40
+    }
+
+    return (
+      <View style={styles.container}>
+        <View>
+          <IconButton
+            size={36}
+            color='white'
+            icon='crop-free'
+            onPress={() => console.log('Pressed')}
+          />
         </View>
-        <View style={styles.keyboardRow}>
-          <Button mode='text' onPress={() => console.log('Pressed')}>
-            <Text style={styles.keyboardButton}>4</Text>
-          </Button>
-          <Button mode='text' onPress={() => console.log('Pressed')}>
-            <Text style={styles.keyboardButton}>5</Text>
-          </Button>
-          <Button mode='text' onPress={() => console.log('Pressed')}>
-            <Text style={styles.keyboardButton}>6</Text>
-          </Button>
+        <Animatable.View
+          ref={this.handleViewRef}
+          style={styles.amountContainer}>
+          <Logo color='white' size={80} />
+          {this.state.amount.map((a, i) => (
+            <Animatable.Text
+              key={i}
+              style={{ ...styles.amountText, fontSize }}
+              animation='fadeInDown'
+              duration={200}>
+              {a}
+            </Animatable.Text>
+          ))}
+        </Animatable.View>
+        <View style={styles.keyboardContainer}>
+          <View style={styles.keyboardRow}>
+            <KeyboardButton onPress={() => this.addAmount(1)} text={1} />
+            <KeyboardButton onPress={() => this.addAmount(2)} text={2} />
+            <KeyboardButton onPress={() => this.addAmount(3)} text={3} />
+          </View>
+          <View style={styles.keyboardRow}>
+            <KeyboardButton onPress={() => this.addAmount(4)} text={4} />
+            <KeyboardButton onPress={() => this.addAmount(5)} text={5} />
+            <KeyboardButton onPress={() => this.addAmount(6)} text={6} />
+          </View>
+          <View style={styles.keyboardRow}>
+            <KeyboardButton onPress={() => this.addAmount(7)} text={7} />
+            <KeyboardButton onPress={() => this.addAmount(8)} text={8} />
+            <KeyboardButton onPress={() => this.addAmount(9)} text={9} />
+          </View>
+          <View style={styles.keyboardRow}>
+            <KeyboardButton onPress={this.addDecimal} text='.' />
+            <KeyboardButton onPress={() => this.addAmount(0)} text='0' />
+            <KeyboardButton onPress={this.removeAmount}>
+              <Icon name='chevron-left' size={30} color='white' />
+            </KeyboardButton>
+          </View>
         </View>
-        <View style={styles.keyboardRow}>
-          <Button mode='text' onPress={() => console.log('Pressed')}>
-            <Text style={styles.keyboardButton}>7</Text>
-          </Button>
-          <Button mode='text' onPress={() => console.log('Pressed')}>
-            <Text style={styles.keyboardButton}>8</Text>
-          </Button>
-          <Button mode='text' onPress={() => console.log('Pressed')}>
-            <Text style={styles.keyboardButton}>9</Text>
-          </Button>
-        </View>
-        <View style={styles.keyboardRow}>
-          <Button mode='text' onPress={() => console.log('Pressed')}>
-            <Text style={styles.keyboardButton}>.</Text>
-          </Button>
-          <Button mode='text' onPress={() => console.log('Pressed')}>
-            <Text style={styles.keyboardButton}>0</Text>
-          </Button>
-          <Button mode='text' onPress={() => console.log('Pressed')}>
-            <Icon name='chevron-left' size={30} color='white' />
-          </Button>
+        <View style={styles.actions}>
+          <TouchableRipple
+            onPress={() => console.log('Pressed')}
+            rippleColor='rgba(0, 0, 0, .32)'
+            style={styles.action}>
+            <Text style={styles.actionText}>Receive</Text>
+          </TouchableRipple>
+          <TouchableRipple
+            onPress={() => console.log('Pressed')}
+            rippleColor='rgba(0, 0, 0, .32)'
+            style={styles.action}>
+            <Text style={styles.actionText}>Send</Text>
+          </TouchableRipple>
         </View>
       </View>
-      <View style={styles.actions}>
-        <TouchableRipple
-          onPress={() => console.log('Pressed')}
-          rippleColor='rgba(0, 0, 0, .32)'
-          style={styles.action}>
-          <Text style={styles.actionText}>Receive</Text>
-        </TouchableRipple>
-        <TouchableRipple
-          onPress={() => console.log('Pressed')}
-          rippleColor='rgba(0, 0, 0, .32)'
-          style={styles.action}>
-          <Text style={styles.actionText}>Send</Text>
-        </TouchableRipple>
-      </View>
-    </View>
-  )
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -98,10 +190,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flexDirection: 'row'
   },
+  amountView: {
+    flexDirection: 'row'
+  },
   amountText: {
     color: 'white',
-    fontSize: 60,
-    paddingRight: 30
+    fontWeight: '800'
   },
   keyboardContainer: {
     flex: 1,
@@ -109,10 +203,13 @@ const styles = StyleSheet.create({
   },
   keyboardRow: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-around'
+    flexDirection: 'row'
   },
   keyboardButton: {
+    flex: 1
+  },
+  keyboardButtonText: {
+    flex: 1,
     color: 'white',
     fontSize: 24,
     fontWeight: '700'
